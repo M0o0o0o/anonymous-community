8dd1cf1a22fd4815dcd1ebb4d101ddee87cb864a
Lee-moo <dlandif22@gmail.com>
2022-09-06 19:48:51 +0900

    Feat change appPostDao

--- dao/addPostDao.js
+++ dao/addPostDao.js
@@ -1,11 +1,14 @@
 const bcrypt = require("bcrypt");
 const { v4: uuidv4 } = require("uuid");
-module.exports = async (body) => {
+const weatherApi = require("../weatherapi");
+
+module.exports = async (body, ip) => {
   return {
     board_id: uuidv4(),
     title: body.title,
     content: body.content,
     password: await bcrypt.hash(body.password, 12),
     nickname: body.nickname !== undefined ? body.nickname : "익명",
+    weather: await weatherApi(ip),
   };
 };
