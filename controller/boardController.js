3c8e579179e3685e50a23117d8cb300befd9d0c5
Lee-moo <dlandif22@gmail.com>
2022-09-07 20:34:04 +0900

    Feat change response status code

--- controller/boardController.js
+++ controller/boardController.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-useless-catch */
+const errorCodes = require("../codes/errorCodes");
+const sucessCodes = require("../codes/sucessCodes");
 const addPostDao = require("../dao/addPostDao");
 const deletePostDao = require("../dao/deletePostDao");
 const putPostDao = require("../dao/putPostDao");
@@ -14,7 +16,7 @@ const addPost = async (req, res, next) => {
     const post = await boardService.addPost(data);
 
     res.location(`/board/${post.board_id}`);
-    res.status(201).end();
+    res.status(201).json({ message: sucessCodes.POSTSUCCESS });
   } catch (err) {
     next(err);
   }
@@ -23,9 +25,12 @@ const getPost = async (req, res, next) => {
   try {
     const { id } = req.params;
     const post = await boardService.getPost(id);
+    if (post === null) {
+      res.status(404);
+      throw new Error(errorCodes.pageNotFound);
+    }
     res.status(200).json(post);
   } catch (err) {
-    res.status(404);
     next(err);
   }
 };
@@ -43,7 +48,7 @@ const setPost = async (req, res, next) => {
     const data = putPostDao(req.params, req.body);
     const updatedPost = await boardService.setPost(data);
     res.location(`/job/${updatedPost.board_id}`);
-    res.status(201).end();
+    res.status(201).json({ message: sucessCodes.PUTSUCCUESS });
   } catch (err) {
     next(err);
   }
@@ -53,9 +58,9 @@ const deletePost = async (req, res, next) => {
     const data = deletePostDao(req.params.id, req.body.password);
     const deletedPost = await boardService.deletePost(data);
     if (deletedPost === 1) {
-      res.status(200).end();
+      res.status(200).json({ message: sucessCodes.DELETESUCCESS });
     }
-    res.status(200).end(0);
+    res.status(404).end({ message: errorCodes.pageNotFound });
   } catch (err) {
     next(err);
   }
